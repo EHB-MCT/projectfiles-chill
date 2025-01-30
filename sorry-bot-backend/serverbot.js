4d9954408833123a099d035eec9f3327d2591a49
Hamza Sahmoudi <hamza.sahmoudi@student.ehb.be>
2025-01-30 18:53:45 +0100

    Refactor copy button implementation and update input field styles for improved usability; add node-fetch dependency

--- sorry-bot-backend/serverbot.js
+++ sorry-bot-backend/serverbot.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const fetch = require("node-fetch");
 
 const app = express();
 const PORT = 3000;
